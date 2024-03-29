1a73dfa9b3f5480cac8c1a8bc499e1be3bc65ac0
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-03-29 17:20:38 +0530

    updated bgChanger added new buttons

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -1,36 +1,104 @@
-import {useState} from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [color, setColor] = useState('olive')
+  const [color, setColor] = useState("olive");
 
   // function changeColor(color){
   //   setColor(color)
   // }
 
   return (
-   <div className='w-full h-screen duration-200 bg-black' style={{backgroundColor: color}}>
-    <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
-      <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
-         <button onClick={()=> setColor('red')}
-         className='outlibe-none px-4 py-1 rounded-full text-white shadow-lg text-black' style={{backgroundColor: 'red'}}>Red</button>
-         
-         <button onClick={()=> setColor('green')}
-         className='outlibe-none px-4 py-1 rounded-full text-white shadow-lg text-black' style={{backgroundColor: 'green'}}>Green</button>
+    <div
+      className="w-full h-screen duration-200 bg-black"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+            onClick={() => setColor("red")}
+            className="outlibe-none px-4 py-1 rounded-full text-white shadow-lg text-black"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
 
-          <button onClick={()=> setColor('yellow')}
-         className='outlibe-none px-4 py-1 rounded-full text-white shadow-lg text-black' style={{backgroundColor: 'yellow'}}>Yellow</button>
+          <button
+            onClick={() => setColor("green")}
+            className="outlibe-none px-4 py-1 rounded-full text-white shadow-lg text-black"
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
 
-          <button onClick={()=> setColor('blue')}
-         className='outlibe-none px-4 py-1 rounded-full text-white shadow-lg text-black' style={{backgroundColor:'blue'}}>Blue</button>
-         
+          <button
+            onClick={() => setColor("yellow")}
+            className="outlibe-none px-4 py-1 rounded-full text-black shadow-lg text-black"
+            style={{ backgroundColor: "yellow" }}
+          >
+            Yellow
+          </button>
+
+          <button
+            onClick={() => setColor("blue")}
+            className="outlibe-none px-4 py-1 rounded-full text-white shadow-lg text-black"
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
+
+          <button
+            onClick={() => setColor("gray")}
+            className="outlibe-none px-4 py-1 rounded-full text-white shadow-lg text-black"
+            style={{ backgroundColor: "gray" }}
+          >
+            Gray
+          </button>
+
+          <button
+            onClick={() => setColor("purple")}
+            className="outlibe-none px-4 py-1 rounded-full text-white shadow-lg text-black"
+            style={{ backgroundColor: "purple" }}
+          >
+            Purple
+          </button>
+
+          <button
+            onClick={() => setColor("pink")}
+            className="outlibe-none px-4 py-1 rounded-full text-black shadow-lg text-black"
+            style={{ backgroundColor: "pink" }}
+          >
+            Pink
+          </button>
+
+          <button
+            onClick={() => setColor("lavender")}
+            className="outlibe-none px-4 py-1 rounded-full text-black shadow-lg text-black"
+            style={{ backgroundColor: "lavender" }}
+          >
+            Lavender
+          </button>
+          
+          <button
+            onClick={() => setColor("white")}
+            className="outlibe-none px-4 py-1 rounded-full text-black shadow-lg text-black"
+            style={{ backgroundColor: "white" }}
+          >
+            White
+          </button>
+
+          <button
+            onClick={() => setColor("black")}
+            className="outlibe-none px-4 py-1 rounded-full text-white shadow-lg text-black"
+            style={{ backgroundColor: "black" }}
+          >
+            Black
+          </button>
+        </div>
       </div>
-
     </div>
-
-   </div>
-  )
+  );
 }
 
-export default App
+export default App;
